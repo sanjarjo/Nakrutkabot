eb88b550c569b482dade634a25fcd299a2699d8c
sanjarjo <osanjar642@gmail.com>
2025-12-11 16:36:48 +0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -2,7 +2,7 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
     keyboard = [
-        [KeyboardButton("🛒 Xizmatlar"), KeyboardButton("💳 Hisobim")],
-        [KeyboardButton("📥 To'lov qilish"), KeyboardButton("📦 Buyurtmalarim")]
+        [KeyboardButton("📦 Xizmatlar"), KeyboardButton("👤 Hisobim")],
+        [KeyboardButton("💳 To'lov qilish"), KeyboardButton("🧾 Buyurtmalarim")]
     ]
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
