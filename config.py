e67ca38072da4bdcef25377aefcf62240bab9c71
sanjarjo <osanjar642@gmail.com>
2025-12-15 23:54:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 SMM_API_URL = "https://uzbek-seen.uz/api/v2"
 SMM_API_KEY = os.getenv("SMM_API_KEY")
 
-# PostgreSQL uchun
-DATABASE_URL = os.getenv("DATABASE_URL")  # Render env variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 ORDER_CHECK_INTERVAL = int(os.getenv("ORDER_CHECK_INTERVAL", 300))  # 5 daqiqa
