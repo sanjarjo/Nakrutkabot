5fcb61547f4802cc9850967cb46a3f29ea6b8444
sanjarjo <osanjar642@gmail.com>
2025-12-15 23:55:14 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,16 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    future=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
