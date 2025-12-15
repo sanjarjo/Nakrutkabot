dd4c2070e0ce610a2968b33188d321a1ceed676a
sanjarjo <osanjar642@gmail.com>
2025-12-15 22:51:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 from telegram.ext import Application
 from config import BOT_TOKEN
 from handlers.start import start_handler
+from database.init_db import init_db
 
+def build_app():
+    init_db()  # 🔥 DB avtomatik yaratiladi
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(start_handler)
+    return app
+    
 def build_app():
     app = Application.builder().token(BOT_TOKEN).build()
 
