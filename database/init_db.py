130a9714efc79c6c8e4b2df52d831b7208470b7a
sanjarjo <osanjar642@gmail.com>
2025-12-15 23:56:01 +0500

    Update init_db.py

--- database/init_db.py
+++ database/init_db.py
@@ -2,5 +2,4 @@ from database.db import engine, Base
 from database.models import *
 
 def init_db():
-    # PostgreSQL’da ham ishlaydi, barcha jadvallarni yaratadi
     Base.metadata.create_all(bind=engine)
