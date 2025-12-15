f3a6ee67835181863e2a5b30077fc3fa438858c1
sanjarjo <osanjar642@gmail.com>
2025-12-16 00:50:17 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
+# psycopg3 bilan ishlash uchun URL ni moslashtiramiz
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
+
 engine = create_engine(DATABASE_URL, echo=False, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
