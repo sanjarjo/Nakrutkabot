eda65fdf0e2ac145ea68b06fb362da0c5ea541bf
sanjarjo <osanjar642@gmail.com>
2025-12-16 18:40:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,20 @@ from flask import Flask, request
 from telegram import Update
 from bot import build_app
 import asyncio
-from asgiref.wsgi import WsgiToAsgi
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 tg_app = build_app()
 
-@flask_app.route("/", methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def home():
     return "SMM Bot ishlayapti 🚀"
 
-@flask_app.route("/webhook", methods=["POST"])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.get_json(force=True)
     update = Update.de_json(data, tg_app.bot)
 
-    asyncio.get_event_loop().create_task(
-        tg_app.process_update(update)
-    )
+    asyncio.run(tg_app.process_update(update))
     return "OK"
-
-# ASGI wrapper
-app = WsgiToAsgi(flask_app)
