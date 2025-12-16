06ac777a8a67f608f9dc051841e1a7ff0d92c5ba
sanjarjo <osanjar642@gmail.com>
2025-12-16 07:31:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,25 @@
-from flask import Flask, request
+from quart import Quart, request
 from telegram import Update
 import asyncio
-
 from bot import build_app
 from config import BOT_TOKEN
 
-app = Flask(__name__)
-
-# 🔹 Telegram Application
+app = Quart(__name__)
 tg_app = build_app()
 
-# 🔹 Event loop
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-# 🔹 Telegram app start
+loop = asyncio.get_event_loop()
 loop.run_until_complete(tg_app.initialize())
 loop.run_until_complete(tg_app.start())
 
 
 @app.route("/", methods=["GET"])
-def home():
+async def home():
     return "✅ SMM Bot ishlayapti (Webhook)"
 
 
 @app.route(f"/webhook/{BOT_TOKEN}", methods=["POST"])
-def webhook():
-    data = request.get_json(force=True)
+async def webhook():
+    data = await request.get_json(force=True)
     update = Update.de_json(data, tg_app.bot)
-    loop.run_until_complete(tg_app.process_update(update))
+    await tg_app.process_update(update)
     return "OK", 200
