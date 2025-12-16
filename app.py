a1e12b3b62125348fe7072a7f4588cc6210bdeb7
sanjarjo <osanjar642@gmail.com>
2025-12-16 19:12:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from bot import create_bot
 import asyncio
 
 app = Flask(__name__)
-tg_app = build_app()
 
+tg_app = create_bot()
 
 @app.route("/", methods=["GET"])
 def home():
     return "SMM Bot ishlayapti 🚀"
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
